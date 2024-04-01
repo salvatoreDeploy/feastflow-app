12dcbcd50b28e9a3c19bfb09de34eac617fe8d33
salvatoreDeploy <liderhenrique@gmail.com>
2024-04-01 11:43:25 -0300

    component:Página: Login

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,20 @@
+import { Utensils } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autentication</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg  text-foreground">
+          <Utensils className="h-5 w-5" />
+          <span className="bold">FeastFlow</span>
+        </div>
+        <footer>
+          Painel do parceiro &copy; FeastFlow - {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,37 @@
+import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Sign-in" />
-      <h1>Sign-IN</h1>
+      <div className="p-8">
+        <div className="w-[320px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center ">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Acessar Painel de Controle
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe e tenha controle da suas vendas por aqui...
+            </p>
+          </div>
+          <form action="" method="post" className="flex flex-col space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail:</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Digite seu e-mail..."
+              />
+            </div>
+
+            <Button type="button">Acessar painel</Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
