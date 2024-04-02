3f8361c27589eb38763e0035bd82dd5465265edc
salvatoreDeploy <liderhenrique@gmail.com>
2024-04-02 16:12:15 -0300

    component: ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | feastflow" />
-      <RouterProvider router={router}></RouterProvider>
-      <Toaster richColors />
+      <ThemeProvider storageKey="feastflow-theme">
+        <Helmet titleTemplate="%s | feastflow" />
+        <RouterProvider router={router}></RouterProvider>
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
